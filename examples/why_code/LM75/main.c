f0c843dd5af6e07d66651fcedefa554ee0279ed0
why121368 <3033146052@qq.com>
2023-07-06 09:38:48 +0800

    1.新增了LM75传感器驱动bsp/common/LM75，同时将/bsp加入版本控制 2.whycode下新增LM75文件夹

---
+++ examples/why_code/LM75/main.c
@@ -0,0 +1,16 @@
+#include "bflb_mtimer.h"
+#include "board.h"
+#include "lm75.h"
+
+// #define DBG_TAG "MAIN"
+// #include "log.h"
+
+int main(void)
+{
+    float temp_why=0.0;
+    board_init();
+    board_i2c0_gpio_init();
+
+
+    
+}
